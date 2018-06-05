af5577b9288a276d7a4d4364d286654900b8d0cf
sunning <sunning@auto-fis.com>
2018-06-05 16:10:41 +0800

    checkstyle

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Copyright 2018 sunning
+ * 
+ * Licensed under the SUNNING, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.baidu.com
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
 package com.mycompany.app;
 
 import java.util.HashSet;
@@ -6,30 +19,25 @@ import java.util.Set;
 /**
  * Hello world!
  */
-public class App
-{
+public class App {
 
-    private final String message = "Hello World!";
+  private final String message = "Hello World!";
 
-    public App()
-    {
+  public App() {}
+
+  public static void main(String[] args) {
+    System.out.println(new App().getMessage());
+
+    Set<Short> s = new HashSet<>();
+    for (short i = 0; i < 100; i++) {
+      s.add(i);
+      s.remove(i - 1);
     }
+    System.out.println(s.size());
+  }
 
-    public static void main(String[] args)
-    {
-        System.out.println(new App().getMessage());
-
-        Set<Short> s = new HashSet<>();
-        for (short i = 0; i < 100; i++) {
-            s.add(i);
-            s.remove(i - 1);
-        }
-        System.out.println(s.size());
-    }
-
-    private final String getMessage()
-    {
-        return message;
-    }
+  private final String getMessage() {
+    return message;
+  }
 
 }
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Copyright 2018 sunning
+ * 
+ * Licensed under the SUNNING, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.baidu.com
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
 package com.mycompany.app;
 
 import java.io.ByteArrayOutputStream;
@@ -10,39 +23,37 @@ import static org.junit.Assert.*;
 /**
  * Unit test for simple App.
  */
-public class AppTest
-{
+public class AppTest {
 
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+  private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
-    @Before
-    public void setUpStreams() {
-        System.setOut(new PrintStream(outContent));
+  @Before
+  public void setUpStreams() {
+    System.setOut(new PrintStream(outContent));
+  }
+
+  @Test
+  public void testAppConstructor() {
+    try {
+      new App();
+    } catch (Exception e) {
+      fail("Construction failed.");
     }
+  }
 
-    @Test
-    public void testAppConstructor() {
-        try {
-            new App();
-        } catch (Exception e) {
-            fail("Construction failed.");
-        }
+  @Test
+  public void testAppMain() {
+    App.main(null);
+    try {
+      assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
+    } catch (AssertionError e) {
+      fail("\"message\" is not \"Hello World!\"");
     }
+  }
 
-    @Test
-    public void testAppMain()
-    {
-        App.main(null);
-        try {
-            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
-        } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World!\"");
-        }
-    }
-
-    @After
-    public void cleanUpStreams() {
-        System.setOut(null);
-    }
+  @After
+  public void cleanUpStreams() {
+    System.setOut(null);
+  }
 
 }
