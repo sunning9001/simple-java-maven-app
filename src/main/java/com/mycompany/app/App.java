fc2c09c5f2f80b858381b3e7656f26544b885289
sunning <sunning@auto-fis.com>
2018-06-05 14:45:14 +0800

    code template  and  code style

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,8 @@
 package com.mycompany.app;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Hello world!
  */
@@ -8,13 +11,24 @@ public class App
 
     private final String message = "Hello World!";
 
-    public App() {}
-
-    public static void main(String[] args) {
-        System.out.println(new App().getMessage());
+    public App()
+    {
     }
 
-    private final String getMessage() {
+    public static void main(String[] args)
+    {
+        System.out.println(new App().getMessage());
+
+        Set<Short> s = new HashSet<>();
+        for (short i = 0; i < 100; i++) {
+            s.add(i);
+            s.remove(i - 1);
+        }
+        System.out.println(s.size());
+    }
+
+    private final String getMessage()
+    {
         return message;
     }
 
